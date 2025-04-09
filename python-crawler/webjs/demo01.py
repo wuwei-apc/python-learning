64451d0ddf0ed3c1cc77478a58d3e56e3d435821
zhupengcai <zpc@xinhe.pro>
2025-04-10 01:30:09 +0800

    feat: add crawler demo AES

---
+++ python-crawler/webjs/demo01.py
@@ -0,0 +1,33 @@
+"""
+@FileName：demo01.py
+@Description：you description
+@Author：zpc20 
+@productName: PyCharm
+@Time：2024/3/8 12:59
+"""
+import requests
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Referer': 'https://xueqiu.com/today',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+response = requests.get('https://xueqiu.com/today', headers=headers)
+acw_tc = response.cookies.get_dict()
+print(acw_tc)
+
+with open('./demo2.html','w',encoding='utf-8') as f:
+      f.write(response.text)
